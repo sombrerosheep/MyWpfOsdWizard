8b0d1ab863c2cde24b3dd1dbf8fe967dc26e6275
Swanson Noah <SwansonNoah@JohnDeere.com>
2014-11-30 20:40:16 -0600

    Initial Commit

---
+++ Classes/TaskSequenceEnvironment.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+// You'll need to create and import your own DLL from the WinPE Media.
+// See my blog for details: http://wp.me/p4bOHR-E
+using TSEnvironmentLib_x86;
+
+namespace MyWpfOsdWizard.Classes
+{
+    /// <summary>
+    /// Creates an instance of the running Task Sequence Environment.
+    /// Used as a helper class for dealing with Task Sequence Variables.
+    /// </summary>
+    class TaskSequenceEnvironment
+    {
+        ITSEnvClass TsEnvVar = new TSEnvClass();
+
+        /// <summary>
+        /// Sets a specific task Sequence variable
+        /// </summary>
+        /// <param name="variableName">The variable name to set.</param>
+        /// <param name="variableValue">The value to set the variable to.</param>
+        public bool SetTaskSequenceVariable(string variableName, string variableValue)
+        {
+            try
+            {
+                TsEnvVar[variableName] = variableValue;
+            }
+            catch
+            {
+                MessageBox.Show("Unable to set Variable:\n" + variableName + "\n" + variableValue);
+                return false;
+            }
+
+            return true;
+
+        }
+
+        /// <summary>
+        /// Gets the value of a specific Task Sequence Variable
+        /// </summary>
+        /// <param name="variableName">The name of the variable who's value you are getting</param>
+        /// <returns>Value of the variableName parameter.</returns>
+        public string GetTaskSequenceVariable(string variableName)
+        {
+            return TsEnvVar[variableName];
+        }
+
+        /// <summary>
+        /// Returns a Dictionary object of each task sequence Variable Name and Value
+        /// </summary>
+        /// <returns>Dictionay object of all Task Sequence variables. Key is the Variable Name. Value is the variable Value.</returns>
+        public Dictionary<string, string> GetAllTsVariables()
+        {
+            Dictionary<string, string> tsVars = new Dictionary<string, string>();
+
+            object[] x = (object[])TsEnvVar.GetVariables();
+
+            for (int i = 0; i < x.Length; i++)
+            {
+                tsVars.Add(x[i].ToString(), tsVars[x[i].ToString()]);
+            }
+
+            return tsVars;
+        }
+
+    }
+}
---
+++ Pages/Landing.xaml.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MyWpfOsdWizard.Pages
+{
+    /// <summary>
+    /// Interaction logic for Landing.xaml
+    /// </summary>
+    public partial class Landing : Page
+    {
+        public Landing()
+        {
+            InitializeComponent();
+        }
+
+        private void osdComputerName_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            // Empty Computer Name
+            if (osdComputerName.Text.Length < 1)
+            {
+                computerNameErrIcon.Visibility = System.Windows.Visibility.Visible;
+                computerNameErrIcon.ToolTip = "Please enter a Computer Name!";
+                finishButton.IsEnabled = false;
+            }
+            // Computer Name is Too Long
+            else if (osdComputerName.Text.Length > 15)
+            {
+                computerNameErrIcon.Visibility = System.Windows.Visibility.Visible;
+                computerNameErrIcon.ToolTip = "Computer Name cannot exceed 15 characters!";
+                finishButton.IsEnabled = false;
+            }
+            // Computer Name is Good!
+            else
+            {
+                computerNameErrIcon.Visibility = System.Windows.Visibility.Hidden;
+                computerNameErrIcon.ToolTip = "";
+                finishButton.IsEnabled = true;
+            }
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Classes.TaskSequenceEnvironment tsEnv = null;
+
+            // Get instance of the Task Sequence environment.
+            try
+            {
+                tsEnv = new Classes.TaskSequenceEnvironment();
+            }
+            catch
+            {
+                MessageBox.Show("Unable to locate Task Sequence Environment!", "WinPE .NET Wizard");
+                Application.Current.Shutdown(1);
+            }
+
+            // Set the Computer Name
+            tsEnv.SetTaskSequenceVariable("OSDComputerName", osdComputerName.Text);
+
+            //Set Variable One, if present
+            if (osdVarNameOne.Text != "" && osdVarValOne.Text != "")
+            {
+                tsEnv.SetTaskSequenceVariable(osdVarNameOne.Text, osdVarValOne.Text);
+            }
+
+            //Set Variable Two, if present
+            if (osdVarNameTwo.Text != "" && osdVarValTwo.Text != "")
+            {
+                tsEnv.SetTaskSequenceVariable(osdVarNameTwo.Text, osdVarValTwo.Text);
+            }
+
+            //Set Variable Three, if present
+            if (osdVarNameThree.Text != "" && osdVarValThree.Text != "")
+            {
+                tsEnv.SetTaskSequenceVariable(osdVarNameThree.Text, osdVarValThree.Text);
+            }
+
+            //Set Variable Four, if present
+            if (osdVarNameFour.Text != "" && osdVarValFour.Text != "")
+            {
+                tsEnv.SetTaskSequenceVariable(osdVarNameFour.Text, osdVarValFour.Text);
+            }
+
+            //Set Variable Five, if present
+            if (osdVarNameFive.Text != "" && osdVarValFive.Text != "")
+            {
+                tsEnv.SetTaskSequenceVariable(osdVarNameFive.Text, osdVarValFive.Text);
+            }
+
+            // Exit the Wizard
+            Application.Current.Shutdown(0);
+        }
+    }
+}
